1de1bb25db4b95653470e9193e9de1ddd7d6a647
OSangKwon <csu04008@naver.com>
2018-11-30 21:23:00 +0900

    api_user

--- api/user.js
+++ api/user.js
@@ -37,7 +37,7 @@ router.get('/',function(req,res,next){
  function(req,res,next){
     var database= app.get('database');
     var User=database.UserModel;
-     var newUser = new User(req.body);
+     var newUser = new User(req.body.data);
      newUser.id=res.locals.lastId+1;
      newUser.save(function(err,user){
          if(err){
