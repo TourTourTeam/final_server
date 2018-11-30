494ea2ba8b573d8d3024e30549cce1e8291c2c62
OSangKwon <csu04008@naver.com>
2018-11-30 22:10:48 +0900

    api_user put

--- api/user.js
+++ api/user.js
@@ -77,6 +77,7 @@ router.get('/',function(req,res,next){
  router.put('/:id',function(req,res,next){
     var database= app.get('database');
     var User=database.UserModel;
+    console.log('make object');
      User.findOneAndUpdate({id:req.params.id}, JSON.parse(req.body))
      .exec(function(err,user){
          if(err){
